841a44e8723a7fe4573c22f31a5e4eee672ab6e7
Kantaro08 <kankun0811@icloud.com>
2026-06-24 23:42:06 +0900

    Improve question suggestions in sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='hitolog-v22';
+const CACHE='hitolog-v23';
 const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./privacy.html'];
 self.addEventListener('install',event=>event.waitUntil(Promise.all([
   self.skipWaiting(),
